1f469fde8909f790783a5733acd545e31f8707d9
eduarchilon <eduarchilon@outlook.es>
2021-11-19 13:37:57 -0300

    Primera version

---
+++ app.js
@@ -0,0 +1,54 @@
+$(window).resize(function() {
+    // var heightBrowser =$(window).height();
+    var widthBrowser =$(window).width();
+    // console.log("Tamaño de la pantalla del navegador: width="+widthBrowser +" height="+heightBrowser );
+    
+    /*condiciono asi aparece*/
+    if(widthBrowser>1024){
+        $('.menu-login__container').css("display","flex");
+    }else{
+        $('.menu-login__container').css("display","none");
+    }
+ });
+
+
+$(document).ready(function(){
+
+	console.log("aaaaa");
+    let count = 0;
+    /*para los botones del header*/
+	$("#btn-menu").click(function(){
+		if(count===0 ){
+			$('.menu-login__container').show("slow");
+			count=1;
+		}else{
+			$('.menu-login__container').hide("slow");
+			count=0;
+		}
+	  })
+
+      setTimeout(()=>{
+
+
+        $('main').append(`
+        <div class="alerta__fondo">
+        <div class="alerta__container">
+            <button type="button" id="btn-close">X</button>
+            <div class="alerta__titulo">
+                <h3>¡Hola <span>Usuario</span>!</h3>
+            </div>
+            <div class="alerta__mensaje">
+                <p>Recordá que hoy tenes la sesión con la
+                <span>Dra. Acevedo </span>
+                a las 15:00 hs</p>
+            </div>
+            <button type="btn-sesion" id="btn-ir">IR A LA SESIÓN</button>
+        </div>
+    </div>`);
+
+    $('#btn-close').click(function(){
+        $('.alerta__fondo').hide();
+    });
+    }, 2000);
+
+});
